da1d1e65ad173ccb7faf7519e938cc755a2552d3
Jamil-4khtar <156568813+Jamil-4khtar@users.noreply.github.com>
2025-01-01 19:19:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+window.addEventListener("resize", () => {
+	//your JS code here. If required.
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	
+	let oreintation;
+	
+	if (height > width) {
+		orientation = 'portrait';
+	} else {
+		orientation = 'landscape';
+	}
+	
+	let body = document.body;
+	
+	if (orientation === 'portrait') {
+		body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
+	} else {
+		body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
+	}
+})
