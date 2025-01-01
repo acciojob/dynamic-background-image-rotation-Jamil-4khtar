4d78deb46e1cefc91ca2f66f5748d99fab0c0d81
Jamil-4khtar <jamil4khtar@gmail.com>
2025-01-01 20:06:06 +0530

    Refactor background image handling to use CSS media queries for orientation

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-window.addEventListener("resize", () => {
-	//your JS code here. If required.
-	bodyBg()
+// window.addEventListener("resize", () => {
+// 	//your JS code here. If required.
+// 	bodyBg()
 	
-})
+// })
 
-window.addEventListener("DOMContentLoaded", bodyBg)
+// window.addEventListener("DOMContentLoaded", bodyBg)
 
-function bodyBg() {
-	let width = window.innerWidth;
-	let height = window.innerHeight;
+// function bodyBg() {
+// 	let width = window.innerWidth;
+// 	let height = window.innerHeight;
 	
-	let body = document.body;
+// 	let body = document.body;
 	
-	if (height > width) {
-		body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
+// 	if (height > width) {
+// 		body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
 
-	} else {
-		body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
+// 	} else {
+// 		body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
 
-	}
-}
+// 	}
+// }
