c81ced2b1c43aa4daddd788117f63102069c2972
Jamil-4khtar <jamil4khtar@gmail.com>
2025-01-01 20:02:24 +0530

    Add dynamic background image based on window orientation

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 window.addEventListener("resize", () => {
 	//your JS code here. If required.
+	bodyBg()
+	
+})
+
+window.addEventListener("DOMContentLoaded", bodyBg)
+
+function bodyBg() {
 	let width = window.innerWidth;
 	let height = window.innerHeight;
 	
-	let oreintation;
-	
-	if (height > width) {
-		orientation = 'portrait';
-	} else {
-		orientation = 'landscape';
-	}
-	
 	let body = document.body;
 	
-	if (orientation === 'portrait') {
+	if (height > width) {
 		body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')";
+
 	} else {
 		body.style.backgroundImage = "url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')";
+
 	}
-})
+}
